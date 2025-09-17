463f5423c2f25508663a62fb6cde0362721c27ed
mark <okolovmark@gmail.com>
2025-09-17 17:47:20 +0800

    [IMP] Request Body

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 class ModelName(str, Enum):
@@ -10,7 +11,31 @@ class ModelName(str, Enum):
     lenet = "lenet"
 
 
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
 app = FastAPI()
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
 
 
 @app.get("/")
